c31491a8d0b5d078dfe4754f710cae828261aa6b
William Lyon <lyonwj@gmail.com>
2021-05-04 08:13:05 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
             </Center>
             <Center>
             <Text color="white" fontSize="3xl">
-            Using the Official Neo4j GraphQL Library
+            The Official Neo4j GraphQL Library
             </Text>
           </Center>
         </GridItem>
